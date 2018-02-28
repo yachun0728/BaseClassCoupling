001a8bd8ebec951929b6d072c385942a73957c3b
Angel Lu <angel.lu86@titansoft.com.sg>
2018-02-28 23:39:22 +0800

    Test and production code

---
+++ baseclasscoupling/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BaseClassCoupling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BaseClassCoupling")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("38080b19-9e1a-4739-971a-463921f69995")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ baseclasscoupling/UnitTest1.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Castle.Core.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+using Moq;
+
+namespace BaseClassCoupling
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void calculate_LessThanOneYearEmployee_Bonus()
+        {
+            //if my monthly salary is 1200, working year is 0.5, my bonus should be 600
+            var lessThanOneYearEmployee = new LessThanOnYearEmployeeForTest()
+            {
+                Id = 91,
+                //Console.WriteLine("your StartDate should be :{0}", DateTime.Today.AddDays(365/2*-1));
+                Today = new DateTime(2018, 1, 27),
+                StartWorkingDate = new DateTime(2017, 7, 29)
+            };
+
+            var actual = lessThanOneYearEmployee.GetYearlyBonus();
+            Assert.AreEqual(600, actual);
+        }
+    }
+
+    public abstract class Employee
+    {
+        public DateTime StartWorkingDate { get; set; }
+        public DateTime Today { get; set; }
+
+        protected decimal GetMonthlySalary()
+        {
+            Info($"query monthly salary id:{Id}");
+            return Get(Id);
+        }
+
+        public abstract decimal GetYearlyBonus();
+
+        public int Id { get; set; }
+
+        public virtual void Info(string log)
+        {
+            DebugHelper.Info(log);
+        }
+
+        public virtual decimal Get(int Id)
+        {
+            return SalaryRepo.Get(this.Id);
+        }
+    }
+
+    public class LessThanOnYearEmployeeForTest : LessThanOneYearEmployee
+    {
+        public override void Info(string log)
+        {
+        }
+
+        public override decimal Get(int Id)
+        {
+            return 1200;
+        }
+    }
+
+    public class LessThanOneYearEmployee : Employee
+    {
+        public override decimal GetYearlyBonus()
+        {
+            Info("--get yearly bonus--");
+            var salary = this.GetMonthlySalary();
+            Info($"id:{Id}, his monthly salary is:{salary}");
+            return Convert.ToDecimal(this.WorkingYear()) * salary;
+        }
+
+        private double WorkingYear()
+        {
+            Info("--get working year--");
+            var year = (Today - StartWorkingDate).TotalDays / 365;
+            return year > 1 ? 1 : Math.Round(year, 2);
+        }
+
+        
+    }
+    
+    //public static class DebugHelper
+    public static class DebugHelper
+    {
+        public static void Info(string message)
+        {
+            //you can't modified this function
+            throw new NotImplementedException();
+        }
+    }
+
+    //interface IDebuggerHelp
+    //{
+    //    void Info(string v);
+    //}
+
+    interface ISalaryRepo
+    {
+        decimal Get(int id);
+    }
+
+    public static class SalaryRepo
+    {
+        internal static decimal Get(int id)
+        {
+            //you can't modified this function
+            throw new NotImplementedException();
+        }
+    }
+}
